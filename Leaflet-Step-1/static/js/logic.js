f79f5810426a078f81f29d517ac00aafe93c735c
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-09-11 12:43:55 -0500

    Changes

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -15,6 +15,51 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // Read the geoJson file
-d3.json("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson").then(function(data) {
-  console.log(data["features"]);
+d3.json("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", function(data, error) {
+  console.log(data);
+
+  if (error) {
+    throw error;
+  };
 });
+
+// Color based on magnitude
+function magnitudeColor(magnitude) {
+  switch (true) {
+  case magnitude > 6.5:
+    return "yellow";
+  case magnitude > 6:
+    return "red";
+  case magnitde > 3.5:
+    return "orange";
+  default:
+    return "white";
+  }
+}
+
+
+
+// Creating radius
+function radiusFinder(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 5;
+}
+
+// Adding GeoJSON
+L.geoJson(data, )
+
+// Pop-ups and Markers
+
+// Update the legend's innerHTML with the last updated time and station count
+function updateLegend(time, stationCount) {
+  document.querySelector(".legend").innerHTML = [
+    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
+    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
+    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
+    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
+    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
+  ].join("");
+}
